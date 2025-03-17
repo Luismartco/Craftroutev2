29387a79bf1830834e15cc3976af9bb0ed67a723
deimyscamargo <deimyscamargo03@gmail.com>
2025-03-17 14:35:14 -0500

    sistema de autenticacion

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,27 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home');  // Renderiza
-})->name('home');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-Route::get('/login', function () {
-    return Inertia::render('Login'); // Renderiza 
-})->name('login');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/register', function () {
-    return Inertia::render('Register'); // Renderiza 
-})->name('register');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/ingreso', function () {
-    return Inertia::render('Ingreso'); // Renderiza 
-})->name('ingreso');
-
+require __DIR__.'/auth.php';
