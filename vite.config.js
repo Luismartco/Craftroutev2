e77b46f68461c5c015bfb9e3f67b66fe476ceec4
deimyscamargo <deimyscamargo03@gmail.com>
2025-04-28 14:58:11 -0500

    Git Ignore

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        host: 'localhost', // 👈 Esto fuerza el uso de IPv4
-        port: 5173,         // Opcional: puedes especificar el puerto si quieres
+  plugins: [
+    laravel({
+      input: 'resources/js/app.jsx',
+      refresh: true,
+    }),
+    react(),
+  ],
+  server: {
+    host: 'craftroutev2.test',
+    port: 5173,
+    strictPort: true,
+    https: {
+      key: fs.readFileSync('craftroutev2.test+4-key.pem'),
+      cert: fs.readFileSync('craftroutev2.test+4.pem'),
     },
+    hmr: {
+      host: 'craftroutev2.test',
+      protocol: 'wss',
+      clientPort: 5173 // Asegúrate que coincida con el puerto del server
+    }
+  },
 });
